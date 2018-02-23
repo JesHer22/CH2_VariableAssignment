9449f839745dd862e3a3664b017db66af00e1e09
JesHer22 <36181973+JesHer22@users.noreply.github.com>
2018-02-22 18:47:01 -0800

    Add files via upload

---
+++ Ch 2 HW.py
@@ -0,0 +1,22 @@
+import math
+number1_ = 1
+number2_ = 2
+print(number1_ + number2_)
+number1_ = 1
+number2_ = 2
+print(number1_ - number2_)
+number1_ = 1
+number2_ = 2
+print(number1_ * number2_)
+number1_ = 1
+number2_ = 2
+print(number1_ + number2_ //2)
+number1_ = 1
+number2_ = 2
+print(math.fabs(number1_ - number2_))
+number1_ = 1
+number2_ = 2
+print(max(number1_,number2_))
+number1_ = 1
+number2_ = 2
+print(min(number1_,number2_))
